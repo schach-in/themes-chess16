3245855c3142fb0b461e31e3e8a4dd1d259e3f39
Gustaf Mossakowski <gustaf@koenige.org>
2022-09-18 15:26:02 +0200

    settings: read realm from function if no value is given and default_from_setting returns nothing

---
+++ chess16/_functions.inc.php
@@ -0,0 +1,30 @@
+<?php 
+
+/**
+ * chess16 theme
+ * common functions
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/chess16
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2022 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * set a default for realm if it does not exist
+ *
+ * @global array $zz_setting
+ * @return string
+ */
+function mt_chess16_realm_default() {
+	global $zz_setting;
+
+	$path = $zz_setting['request_uri'];
+	if (str_starts_with($path, $zz_setting['base_path']))
+		$path = substr($path, strlen($zz_setting['base_path']));
+	$path = explode('/', trim($path, '/'));
+	return $path[0];
+}
