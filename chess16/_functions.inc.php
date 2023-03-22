a40e2f5f763334557c4873c1a3640eb060013574
Gustaf Mossakowski <gustaf@koenige.org>
2023-03-22 10:35:22 +0100

    use wrap_setting() instead of $zz_setting

--- chess16/_functions.inc.php
+++ chess16/_functions.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/chess16
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2022 Gustaf Mossakowski
+ * @copyright Copyright © 2022-2023 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -16,18 +16,16 @@
 /**
  * set a default for realm if it does not exist
  *
- * @global array $zz_setting
  * @return string
  */
 function mt_chess16_realm_default() {
-	global $zz_setting;
 	global $zz_page;
 
 	if (!empty($zz_page['error_code'])) return 'error';
 
-	$path = $zz_setting['request_uri'];
-	if (str_starts_with($path, $zz_setting['base_path']))
-		$path = substr($path, strlen($zz_setting['base_path']));
+	$path = wrap_setting('request_uri');
+	if (str_starts_with($path, wrap_setting('base_path')))
+		$path = substr($path, strlen(wrap_setting('base_path')));
 	$path = explode('/', trim($path, '/'));
 	return $path[0];
 }
